b6c49a27a21303b0bded131822ff0f74b5f88317
supfn <392084352@qq.com>
2023-08-21 15:34:49 +0800

    Update fetch-with-retry.js

--- sence-code/fetch-with-retry.js
+++ sence-code/fetch-with-retry.js
@@ -1,4 +1,12 @@
-// 实现一个请求函数：fetchWithRetry，要求会最多自动重试 3 次，任意一次成功就直接返回
+// 实现一个请求错误重试函数 fetchWithRetry，要求会最多自动重试 3 次，任意一次成功就直接返回，返回结果为promsie
+
+/**
+ * @param {number} retry 重试次数
+ * @param {number} timeout 重试延迟时间
+ * @returns {Promise}
+ */
+// function fetchWithRetry(retry, timeout){
+// }
 
 function fetchWithRetry(retry = 3, timeout = 800) {
 
