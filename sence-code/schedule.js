23ef5636b7058300f0ec0086adc51cf9f410aaa0
supfn <392084352@qq.com>
2023-08-21 11:12:28 +0800

    Update schedule.js

--- sence-code/schedule.js
+++ sence-code/schedule.js
@@ -1,67 +1,62 @@
+// 设计一个简单的异步任务队列执行器，要求执行并发数为 2
 class Scheduler {
   constructor() {
-    this.limit = 2;
-    this.processingNum = 0;
-    this.pendingTask = [];
+      this.limit = 2;
+      this.processingNum = 0;
+      this.pendingTask = [];
   }
 
-  add(fn) {
-    let task = {};
+  createTask(fn) {
+      let task = {};
 
-    task.promise = new Promise((resolve, reject) => {
-      task.resolve = resolve;
-      task.reject = reject;
-    });
-    task.fn = fn;
+      task.promise = new Promise((resolve, reject) => {
+          task.resolve = resolve;
+          task.reject = reject;
+      });
+      task.fn = fn;
 
-    return this.run(task);
+      return task;
   }
 
-  run(task) {
-    if (this.processingNum === this.limit) {
-      this.pendingTask.push(task);
+  runTask(task) {
+      if (this.processingNum === this.limit) {
+          this.pendingTask.push(task);
+          return task.promise;
+      }
+
+      this.processingNum++;
+      task.fn()
+          .then(data => task.resolve(data))
+          .catch(e => task.reject(e))
+          .finally(() => {
+              this.processingNum--;
+              this.next()
+          })
+
       return task.promise;
-    }
-    this.processingNum++;
-    task.fn()
-      .then(data => {
-        task.resolve(data)
-      })
-      .catch(e => task.reject(e))
-      .finally(() => {
-        this.processingNum--;
-        // 继续取任务执行
-        const task = this.get();
-        if (task) {
-          this.run(task);
-        }
-      })
-    return task.promise;
   }
 
-  get() {
-    let task = null;
-    if (this.pendingTask.length) {
-      task = this.pendingTask.shift();
-    }
-    return task;
+  run(fn){
+    const task = this.createTask(fn);
+    return this.runTask(task);
+  }
+
+  next() {
+      if (this.pendingTask.length) {
+          const task = this.pendingTask.shift();
+          return this.runTask(task);
+      }
   }
 }
 
+function test() {
+  const generatePromise = (time, data) => new Promise(resolve => setTimeout(resolve, time, data));
+  const scheduler = new Scheduler();
 
-const timeout = (time) => new Promise(resolve => {
-  setTimeout(resolve, time)
-})
-
-const scheduler = new Scheduler()
-
-const addTask = (time, order) => {
-  scheduler.add(() => timeout(time)).then(() => console.log(order))
+  scheduler.run(() => generatePromise(400,4)).then(data=>console.log(data));
+  scheduler.run(() => generatePromise(200,2)).then(data=>console.log(data));
+  scheduler.run(() => generatePromise(400,3)).then(data=>console.log(data));
+  scheduler.run(() => generatePromise(100,1)).then(data=>console.log(data));
 }
 
-addTask(400, 4)
-addTask(200, 2)
-addTask(400, 3)
-addTask(100, 1)
-//2 4 1 3
-
+test(); // 打印: 2 4 1 3
