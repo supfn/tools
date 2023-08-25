963636a262008054619947f52183e33d45acd430
supfn <392084352@qq.com>
2023-08-25 11:28:43 +0800

    Update compile-template-string.js

--- sence-code/compile-template-string.js
+++ sence-code/compile-template-string.js
@@ -1,3 +1,20 @@
+/* 
+// 题目：实现一个具有模板字符串功能的函数
+function compile(str, data) {
+
+}
+function test() {
+  const data = {
+    b: 'c',
+    d: [{ e: 123 }]
+  };
+  const str = 'hello, ${b}, ${d[0].e}';
+  let ret = compile(str, data); 
+  console.log(ret); // 'hello, c, 123'
+}
+test();
+*/
+
 /**
  * 实现模板字符串
  * 
@@ -5,7 +22,6 @@
  * @param {Object} data 
  * @returns 
  */
-
 function compile(str, data) {
   let reg = /\${(.*?)}/;
   let match = str.match(reg);
@@ -30,8 +46,7 @@ function test() {
     d: [{ e: 123 }]
   };
   const str = 'hello, ${b}, ${d[0].e}';
-  let ret = compile(str, a); // 'hello, c, 123'
-  console.log(ret);
+  let ret = compile(str, a); 
+  console.log(ret); // 打印 'hello, c, 123'
 }
-
 test();
